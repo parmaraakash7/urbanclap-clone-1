493db2feec02006d51d372ee82c8561aa8489905
Aakash Parmar <60379362+parmaraakash7@users.noreply.github.com>
2021-10-17 15:00:39 +0530

    .

--- src/main/java/com/example/demo/utils/ConnectionModel.java
+++ src/main/java/com/example/demo/utils/ConnectionModel.java
@@ -7,22 +7,9 @@ import java.net.*;
 
 public class ConnectionModel {
 	static public int FIVE_KM = 2000;
-<<<<<<< HEAD
-	static public Connection getConnection() throws SQLException {
+	static public Connection getConnection() throws Exception {
 		Connection c = null;
 		c = DriverManager.getConnection("jdbc:postgresql://localhost:5432/urbanclap-clone1","postgres", "aakash@123");
-=======
-	static public Connection getConnection() throws Exception {
- 		Connection c = null;
-// 		c = DriverManager.getConnection("jdbc:postgresql://localhost:5432/urbanclap-clone1","postgres", "aakash@123");
-		URI dbUri = new URI(System.getenv("DATABASE_URL"));
-
-		    String username = dbUri.getUserInfo().split(":")[0];
-		    String password = dbUri.getUserInfo().split(":")[1];
-		    String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + ':' + dbUri.getPort() + dbUri.getPath() + "?ssl=true&sslfactory=org.postgresql.ssl.NonValidatingFactory";
-            
-            	c = DriverManager.getConnection(dbUrl, username, password);
->>>>>>> 32d2268ab003ac60ab8ec0b9c7bcbe21926bbc0c
 		return c;
 		
 	}
